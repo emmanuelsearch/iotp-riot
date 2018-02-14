b3387d60f349b5b06ec5b86af0068670613dab97
firsti <brehmedu@fim.uni-passau.de>
2018-02-14 10:34:53 +0100

    Replaced RIOT with OpenSSL

--- binding.gyp
+++ binding.gyp
@@ -2,7 +2,7 @@
   "targets": [
     {
       "target_name": "addon",
-      "include_dirs": ['RIOT/sys', 'RIOT/sys/include'],
+      "include_dirs": ['/usr/include'],
       "sources": [ "node_bridge.cpp", "eid.h", "eid.c"],
       "defines": [ "SERVER" ]
     }
--- eid.c
+++ eid.c
@@ -10,20 +10,20 @@
 #include <string.h>
 #include <math.h>
 
-#include "include/crypto/ciphers.h"
-#include "crypto/ciphers.c"
-#include "crypto/aes.c"
-#include "crypto/modes/ecb.c"
 #include "eid.h"
 
+#include <openssl/conf.h>
+#include <openssl/evp.h>
+#include <openssl/err.h>
+
 int modulus(double a, double b) {
-	int result = (int) ( a / b );
-	return a - ( result ) * b;
+	int result = (int) (a / b);
+	return a - (result) * b;
 }
 
 void printHex(uint8_t *hex, int offset) {
-	for(int i = 0; i < AES_BLOCK_SIZE; ++i) {
-		if(offset > 0 && i >= offset) {
+	for (int i = 0; i < 16; ++i) {
+		if (offset > 0 && i >= offset) {
 			break;
 		} else {
 			printf("%02x ", hex[i]);
@@ -33,82 +33,96 @@ void printHex(uint8_t *hex, int offset) {
 }
 
 uint8_t *parseIk(char *ikString) {
-	uint8_t ik[16] = {0};
+	uint8_t ik[16] = { 0 };
 	char subString[3];
 	subString[3] = '\0';
 	int j = 0;
-	for(int i = 0; i < 16*2; i+=2) {
+	for (int i = 0; i < 16 * 2; i += 2) {
 		memcpy(subString, &ikString[i], 2);
 		unsigned int intVal;
-		if (sscanf (subString, "%xu", &intVal)!=1) {
-		    fprintf(stderr, "error - not a hex integer");
+		if (sscanf(subString, "%xu", &intVal) != 1) {
+			fprintf(stderr, "error - not a hex integer");
 		}
 		ik[j] = intVal;
 		++j;
 	}
 
 	uint8_t *result = malloc(sizeof(ik));
-	if(result != 0) {
+	if (result != 0) {
 		memmove(result, ik, sizeof(ik));
 	}
 
 	return result;
 }
 
-int generateEID(char *ikString, int scaler, int beacon_time_seconds, uint8_t *eid) {
+int generateEID(char *ikString, int scaler, int beacon_time_seconds,
+		uint8_t *eid) {
+
 	uint8_t *ik = parseIk(ikString);
 	printf("Init Key Data: ");
 	printHex(ik, 0);
 
-	uint8_t tkdata[AES_BLOCK_SIZE] = {
-			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, //"\x00" * 11
+	uint8_t tkdata[16] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, //"\x00" * 11
 			0xff,	//"\xFF"
 			0x00, 0x00, //"\x00" * 2
 			modulus((double) (beacon_time_seconds / (pow(2, 24))), 256), //chr((beacon_time_seconds / (2 ** 24)) % 256)
 			modulus((double) (beacon_time_seconds / (pow(2, 16))), 256) //chr((beacon_time_seconds / (2 ** 16)) % 256)
-	};
+			};
 	printf("Temporary Key Data: ");
 	printHex(tkdata, 0);
 
-	cipher_t tkdataCipher;
-	uint8_t tk[AES_KEY_SIZE] = {0};
-	cipher_init(&tkdataCipher, CIPHER_AES_128, ik, AES_KEY_SIZE);
-	cipher_encrypt(&tkdataCipher, tkdata, tk);
+	unsigned char tk[128];
+	encrypt(tkdata, 16, ik, tk);
 	printf("Temporary Key: ");
 	printHex(tk, 0);
 
 	beacon_time_seconds = (beacon_time_seconds / pow(2, 0)) * pow(2, 0);
-	uint8_t eiddata[AES_BLOCK_SIZE] = {
-			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, //"\x00" * 11
-			scaler,
-			modulus((double) (beacon_time_seconds / (pow(2, 24))), 256),
+	uint8_t eiddata[16] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00,
+			0x00, //"\x00" * 11
+			scaler, modulus((double) (beacon_time_seconds / (pow(2, 24))), 256),
 			modulus((double) (beacon_time_seconds / (pow(2, 16))), 256),
-			modulus((double) (beacon_time_seconds / (pow(2, 8))), 256),
-			modulus((double) (beacon_time_seconds / (pow(2, 0))), 256)
-	};
+			modulus((double) (beacon_time_seconds / (pow(2, 8))), 256), modulus(
+					(double) (beacon_time_seconds / (pow(2, 0))), 256) };
 	printf("Ephermal Id Data: ");
 	printHex(eiddata, 0);
 
-	cipher_t tkEidCipher;
-	cipher_init(&tkEidCipher, CIPHER_AES_128, tk, AES_KEY_SIZE);
-	cipher_encrypt(&tkEidCipher, eiddata, eid);
+	encrypt(eiddata, 16, tk, eid);
 	printf("Ephermal Id: ");
 	printHex(eid, 0);
 
 	return 0;
 }
 
+int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
+		unsigned char *ciphertext) {
+	ERR_load_crypto_strings();
+	OpenSSL_add_all_algorithms();
+	OPENSSL_config(NULL);
 
+	EVP_CIPHER_CTX *ctx;
 
+	int len;
 
+	int ciphertext_len;
 
+	if (!(ctx = EVP_CIPHER_CTX_new()))
+		handleErrors();
 
+	if (1 != EVP_EncryptInit_ex(ctx, EVP_aes_128_ecb(), NULL, key, NULL))
+		handleErrors();
 
+	if (1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
+		handleErrors();
+	ciphertext_len = len;
 
+	if (1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len))
+		handleErrors();
+	ciphertext_len += len;
 
+	EVP_CIPHER_CTX_free(ctx);
 
-
-
-
-
+	return ciphertext_len;
+}
 
