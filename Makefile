5446e27b0b83d720363c17b0e3acd229a9288bff
firsti <brehmedu@fim.uni-passau.de>
2018-03-12 08:39:08 +0100

    Write to inernal flash page and read from it

--- Makefile
+++ Makefile
@@ -8,11 +8,27 @@ QUIET ?= 1
 CFLAGS += -DDEVELHELP
 CFLAGS += -I$(RIOT_DIR)/sys
 CFLAGS += -DCRYPTO_AES
+CFLAGS += -Wno-unused-variable
+
+FEATURES_REQUIRED += periph_flashpage
+FEATURES_OPTIONAL += periph_flashpage_raw
 
 USEMODULE += shell
 USEMODULE += shell_commands
 USEMODULE += ps
 
+USEMODULE += vfs
+USEMODULE += mtd
+
+USEMODULE += littlefs
+USEMODULE += constfs
+
+ifneq (,$(filter littlefs, $(USEMODULE)))
+  CFLAGS += -DVFS_FILE_BUFFER_SIZE=52 -DVFS_DIR_BUFFER_SIZE=44
+else ifneq (,$(filter spiffs, $(USEMODULE)))
+  SPIFFS_NB_FD ?= 8
+  CFLAGS += '-DSPIFFS_FS_FD_SPACE_SIZE=(32 * $(SPIFFS_NB_FD))'
+endif
 
 include $(RIOTBASE)/Makefile.include
 
--- main.c
+++ main.c
@@ -11,6 +11,78 @@
 #include <string.h>
 #include "shell.h"
 
+#include "periph/flashpage.h"
+
+#define LINE_LEN (16)
+
+static void dumpchar(uint8_t mem)
+{
+    if (mem >= ' ' && mem <= '~') {
+        printf("  %c  ", mem);
+    }
+    else {
+        printf("  ?? ");
+    }
+}
+
+static void memdump(void *addr, size_t len)
+{
+    unsigned pos = 0;
+    uint8_t *mem = (uint8_t *)addr;
+
+    while (pos < (unsigned)len) {
+        for (unsigned i = 0; i < LINE_LEN; i++) {
+            printf("0x%02x ", mem[pos + i]);
+        }
+        puts("");
+        for (unsigned i = 0; i < LINE_LEN; i++) {
+            dumpchar(mem[pos++]);
+        }
+        puts("");
+    }
+}
+
+int read_internal(int argc, char **argv) {
+    if(argc > 1) {
+      uint8_t page_mem[FLASHPAGE_SIZE];
+      void *addr;
+      int page;
+      addr = flashpage_addr(atoi(argv[1]));
+      page = flashpage_page(addr);
+
+      flashpage_read(page, page_mem);
+      printf("Read flash page %i into local page buffer\n", page);
+      puts("Local page buffer:");
+      memdump(page_mem, FLASHPAGE_SIZE);
+    }
+    return 0;
+
+}
+
+int write_internal(int argc, char **argv) {
+    if(argc > 2) {
+      uint8_t page_mem[FLASHPAGE_SIZE];
+      void *addr;
+      void *data = argv[1];
+      int page;
+      addr = flashpage_addr(atoi(argv[2]));
+      page = flashpage_page(addr);
+      flashpage_write(page, NULL);
+      printf("successfully erased page %i (addr %p)\n",
+           page, flashpage_addr(page));
+      memcpy(&page_mem, data, strlen(data));
+      if (flashpage_write_and_verify(page, page_mem) != FLASHPAGE_OK) {
+        printf("error: verification for page %i failed\n", page);
+   	return 1;
+      }
+
+      printf("wrote local page to flash page %i at addr %p\n",
+           page, flashpage_addr(page));
+      return 0;
+    }
+    return 1;
+}
+
 int genEID(int argc, char **argv)
 {
     (void) argc;
@@ -29,14 +101,29 @@ int genEID(int argc, char **argv)
     return 0;
 }
 
+static int cmd_info(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+
+    printf("Flash start addr:\t0x%08x\n", (int)CPU_FLASH_BASE);
+    printf("Page size:\t\t%i\n", (int)FLASHPAGE_SIZE);
+    printf("Number of pages:\t%i\n", (int)FLASHPAGE_NUMOF);
+
+    return 0;
+}
+
 static const shell_command_t commands[] = {
         { "geneid", "Generates an EID", genEID },
+        { "info", "Show information about pages", cmd_info },
+        { "write_internal", "Write a string to internal flashpage", write_internal },
+        { "read_internal", "Read internal flashpage", read_internal },
         { NULL, NULL, NULL }
 };
 
-
 int main (void) {
     char line_buf[SHELL_DEFAULT_BUFSIZE];
+    cmd_info(0, NULL);
     shell_run(commands, line_buf, SHELL_DEFAULT_BUFSIZE);
     return 0;
 }
